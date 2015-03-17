71b36523d95355289129a6d4e09acba0af95baee
Harrison Lingren <harrisonlingren@gmail.com>
2015-03-17 13:25:19 -0400

    updated

--- src/Customer.java
+++ src/Customer.java
@@ -5,72 +5,49 @@ import java.util.*;
 Defines the Customer object for use in the La Food program.
 */
 
-public class Fish implements Comparable
+public class Customer
 {
-  // fields
-  String name;
+  String partyName;
   String color;
   String species;
   int fins;
   
   public static int count=0;
-  
-  public int compareTo(Object x)
-  {
-    if(x instanceof Fish)
-	{
-	  Fish xfish=(Fish) x;
-	  return this.name.compareTo(xfish.name);
-	  /*
-	  if(this.fins < xfish.fins)
-	    return -1;
-	  if(this.fins==xfish.fins)
-	    return 0;
-	  return 1;
-	  */
-	}
-	else
-	  System.out.println("Error not a fish!");
-    return 0; 
- }
-  
-  // constructor
-  /**
-  Default basic constructor
-  */
-  public Fish()
+
+  /** Default basic constructor  */
+  public Customer()
   {
     name=""; color=""; species="";
-	fins=0;
-	count++;
+	  fins=0;
+	  count++;
   }
+  
   /**
-  @param n name of the fish
-  @param c color of the fish
-  @param s species of the fish
-  @param f number of fins, if applicable
+    @param n name of the fish
+    @param c color of the fish
+    @param s species of the fish
+    @param f number of fins, if applicable
   */
-  public Fish(String n,String c,String s,
-    int f)
+  
+  public Customer(String n,String c,String s, int f)
   {
     name=n; color=c; species=s; fins=f;
-	count++;
+	  count++;
   }
-  /**
-  @param s Scanner (connected to a text file)
-    to read in to initialize the fish
-  */
+  
+  /** @param s Scanner (connected to a text file)
+      to read in to initialize the customer */
+      
   public Fish(Scanner s)
   {
     name=s.next();
-	color=s.next();
-	species=s.next();
-	fins=s.nextInt();
+	  color=s.next();
+	  species=s.next();
+	  fins=s.nextInt();
   }
-  
-  // accessors
+
   /**
-  @return the name of the fish
+    @return the name of the customer
   */
   public String getName()
     { return name; }
@@ -83,15 +60,7 @@ public class Fish implements Comparable
 	
   public String toString()
   {
-    return name+" color="+color+
-	  " species="+species+" #fins="+fins;
+    return name+" color="+color+" species="+species+" #fins="+fins;
   }
-  
-  // mutator or transformer or setter
-  public void sharkattack()
-  {
-    if(fins>0) fins--;
-  }
-	
 
 }
--- src/lafood.java
+++ src/lafood.java
@@ -1,1 +1,26 @@
-/** file */
+import java.io.*
+import java.util.*
+
+public class LaFood
+{
+  
+  Queue waitList = new QueueLL();
+  
+  public void readFile()
+  {
+    Scanner input=new Scanner(new FileReader("data.txt"));
+    
+    
+  }
+  
+  
+  public static void main(String [] args)
+  {
+    
+  }
+  
+  
+  
+  
+  
+}
